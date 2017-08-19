33eba193eddc45001d80320b8710474e76b3139e
ZonaClark <zona.clark56@gmail.com>
2017-08-18 21:52:13 -0400

    BST

---
+++ is_this_BST.py
@@ -0,0 +1,4 @@
+'''
+Given the root node of a binary tree, can you determine if it's also a binary search tree?
+https://www.hackerrank.com/challenges/ctci-is-binary-search-tree?h_r=next-challenge&h_v=zen
+'''
