d803698ee95cfef5f6dfde8eb1b7750e511a79e2
ZonaClark <zona.clark56@gmail.com>
2017-08-19 23:01:00 -0400

    v0.0

--- is_this_BST.py
+++ is_this_BST.py
@@ -1,4 +1,16 @@
 '''
 Given the root node of a binary tree, can you determine if it's also a binary search tree?
+The function has 1 parameter, which is a pointer to the root of a binary tree.
+The function should return a boolean denoting whether or not the binary tree is a BST. 
+
 https://www.hackerrank.com/challenges/ctci-is-binary-search-tree?h_r=next-challenge&h_v=zen
+
+Node is defined as
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
 '''
+
+def checkBST(root):
