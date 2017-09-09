4c0870a7947cc4c611e663afa73eb70ab0f24dec
ZonaClark <zona.clark56@gmail.com>
2017-09-09 16:03:58 -0400

    v1.0

--- is_this_BST.py
+++ is_this_BST.py
@@ -8,11 +8,43 @@ Note: We do not consider a binary tree to be a binary search tree if it contains
 https://www.hackerrank.com/challenges/ctci-is-binary-search-tree?h_r=next-challenge&h_v=zen
 
 Node is defined as
-class node:
+class Node:
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 '''
+# Assumption: 
+# The given tree is balanced;
+# The data of the node is an int;
 
-def checkBST(root):
+# Note: Binary search trees usually use inorder traversal, because in that way the printed tree will have the numbers in order from the smallest to the biggest. 
+# 		Do null printer checks
+
+arr = []
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def checkBST(Node root):
+	a = inOrderTraversal(root)
+	first = a[0]
+	for i in xrange(1, len(a)):
+		if first >= a[i]:
+			return "No"
+		else:
+			first = a[i]
+	return "Yes"
+
+
+# Inorder traversal
+def inOrderTraversal(Node root):
+	if (root.left != null):
+		inOrderTraversal(root.left)
+	arr.append(root.data)
+	if (root.right != null):
+		inOrderTraversal(root.right)
+	return arr
