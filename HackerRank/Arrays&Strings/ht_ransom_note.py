f5a8a7e8ed0e68be88f16f3399142f25311c820a
ZonaClark <zona.clark56@gmail.com>
2017-11-21 17:40:18 -0500

    hash table v1.5

--- HackerRank/Arrays&Strings/ht_ransom_note.py
+++ HackerRank/Arrays&Strings/ht_ransom_note.py
@@ -1,5 +1,12 @@
 def ransom_note(magazine, ransom):
-	
+	# Hash magazine word and insert them into table
+	table = [[] for _ in range(30)]
+	for w in magazine:
+		hash_num = hash_func(w)
+		table[hash_num].append(w)
+	# For each word in ransom note, check if magazine table has that word
+	for r in random:
+		
 
 def hash_func(x):
 	num = 0
