d250f3efd41aa14c36bdc22d0b4041983f4077d8
ZonaClark <zona.clark56@gmail.com>
2017-11-22 17:51:53 -0500

    pass

--- HackerRank/Arrays&Strings/ht_ransom_note.py
+++ HackerRank/Arrays&Strings/ht_ransom_note.py
@@ -1,30 +1,19 @@
 def ransom_note(magazine, ransom):
-	# Hash magazine word and insert them into table
-	table = [[] for _ in range(30)]
-	for w in magazine:
-		hash_num = hash_func(w)
-		table[hash_num].append(w)
-	# For each word in ransom note, check if magazine table has that word
-	for r in ransom:
-		num = hash_func(r)
-		if not table[num]:
-			return False
-		for word in table[num]:
-			flag = True
-			if word == r:
-				# Find the same word in magazine, delete the word
-				table[num].remove(word)
-				flag = False
-			if flag:
-				return False
-	return True
+	rc = {} # dict of word: count of that word in the note
+    for word in ransom:
+        if word not in rc:
+            rc[word] = 0
+        rc[word] += 1
+    
+    for word in magazine:
+        if word in rc:
+            rc[word] -= 1
+            if rc[word] == 0:
+                del rc[word]
+                if not rc:
+                    return True
+    return False
 
-
-def hash_func(x):
-	num = 0
-	for c in list(x):
-		num += ord(c)
-	return num % 30
 
 m, n = map(int, input().strip().split(' '))
 magazine = input().strip().split(' ')
