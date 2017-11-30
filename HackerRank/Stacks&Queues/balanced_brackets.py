5737ae284f4588d8416122b49aa361fb7ebd92bf
ZonaClark <zona.clark56@gmail.com>
2017-11-30 09:53:10 -0500

    1 attempt

--- HackerRank/Stacks&Queues/balanced_brackets.py
+++ HackerRank/Stacks&Queues/balanced_brackets.py
@@ -1,5 +1,24 @@
 def is_matched(expression):
-	pass
+	stack = []
+	for c in expression:
+		stack.append(c)
+	size = len(stack)
+	if size % 2 != 0:
+		return False
+	first_half = []
+	second_half = []
+	half_size = int(size/2)
+	for c in range(0, half_size):
+		first_half.append(stack[c])
+	for c in range(half_size, size):
+		c = stack.pop()
+		if c == ")":
+			second_half.append("(")
+		if c == "]":
+			second_half.append("[")
+		if c == "}":
+			second_half.append("{")
+	return first_half == second_half
 
 t = int(input().strip())
 for a0 in range(t):
