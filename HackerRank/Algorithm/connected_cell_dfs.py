2e556c570a2caf4028f98ae3a08d67e818e7d250
ZonaClark <zona.clark56@gmail.com>
2017-12-20 16:19:14 -0500

    start dfs

---
+++ HackerRank/Algorithm/connected_cell_dfs.py
@@ -0,0 +1,10 @@
+def getBiggestRegion(grid):
+    
+
+n = int(input().strip())
+m = int(input().strip())
+grid = []
+for grid_i in range(n):
+    grid_t = list(map(int, input().strip().split(' ')))
+    grid.append(grid_t)
+print(getBiggestRegion(grid))
