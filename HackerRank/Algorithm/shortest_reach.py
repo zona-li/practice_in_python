80ff8fc55ee13cc850de165cc5e82930bc0f1259
ZonaClark <zona.clark56@gmail.com>
2017-12-22 15:56:37 -0500

    add shortest reach in a graph

---
+++ HackerRank/Algorithm/shortest_reach.py
@@ -0,0 +1,12 @@
+def find_all_distances(self):
+	pass
+
+t = int(input())
+for i in range(t):
+    n,m = [int(value) for value in input().split()]
+    graph = Graph(n)
+    for i in range(m):
+        x,y = [int(x) for x in input().split()]
+        graph.connect(x-1,y-1) 
+    s = int(input())
+    graph.find_all_distances(s-1)
