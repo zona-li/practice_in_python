5d9e173ae80a41813f1e0751856f9d5e91ae7580
ZonaClark <zona.clark56@gmail.com>
2017-12-28 12:03:17 -0500

    pass sample test

--- HackerRank/Algorithm/primality.py
+++ HackerRank/Algorithm/primality.py
@@ -1,3 +1,27 @@
+import math
+from math import sqrt
+
+
+def isPrime(p):
+	if p < 2:
+		return False
+	elif p == 3:
+		return True
+	elif p % 2 == 0 or p % 3 == 0:
+		return False
+	else:
+		for i in range(3, math.ceil(sqrt(p)), 2):
+			if p % i == 0:
+				return False
+	return True
+
+
+
 p = int(input().strip())
 for a0 in range(p):
 	n = int(input().strip())
+	if isPrime(n):
+		print("Prime")
+	else:
+		print("Not prime")
+
