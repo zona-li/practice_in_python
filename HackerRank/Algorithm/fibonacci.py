69e749c590dec659a10a5adb144d2bf706755d00
ZonaClark <zona.clark56@gmail.com>
2017-12-28 12:36:21 -0500

    pass

--- HackerRank/Algorithm/fibonacci.py
+++ HackerRank/Algorithm/fibonacci.py
@@ -1,4 +1,8 @@
 def fibonacci(n):
-    # Write your code here.
+    if n == 0:
+    	return 0
+    if n == 1:
+    	return 1
+    return fibonacci(n-2) + fibonacci(n-1)
 n = int(input())
 print(fibonacci(n))
