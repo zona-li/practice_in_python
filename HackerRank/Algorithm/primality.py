641d96f2a3ff5ad4e13b366c4119f652c1edeb75
ZonaClark <zona.clark56@gmail.com>
2017-12-28 12:14:34 -0500

    pass

--- HackerRank/Algorithm/primality.py
+++ HackerRank/Algorithm/primality.py
@@ -5,12 +5,12 @@ from math import sqrt
 def isPrime(p):
 	if p < 2:
 		return False
-	elif p == 3:
+	elif p ==2 or p == 3:
 		return True
 	elif p % 2 == 0 or p % 3 == 0:
 		return False
 	else:
-		for i in range(3, math.ceil(sqrt(p)), 2):
+		for i in range(3, int(sqrt(p)+1), 2):
 			if p % i == 0:
 				return False
 	return True
