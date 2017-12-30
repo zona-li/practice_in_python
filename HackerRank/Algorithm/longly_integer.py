ff7e2b8d2b24ee305533a0c3b688b2cedc21c936
ZonaClark <zona.clark56@gmail.com>
2017-12-30 18:31:32 -0500

    add solution2

--- HackerRank/Algorithm/longly_integer.py
+++ HackerRank/Algorithm/longly_integer.py
@@ -12,6 +12,13 @@ def lonely_integer(a):
     		s.add(i)
     return(s.pop())
 
+# Second solution
+def lonely_integer(a):
+    result = 0
+    for i in a:
+        result ^= i
+    return result
+
 n = int(input().strip())
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
 print(lonely_integer(a))
