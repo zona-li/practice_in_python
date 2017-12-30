5879f643ec03a4155893836b2ba2299fa77b6dda
ZonaClark <zona.clark56@gmail.com>
2017-12-30 18:29:21 -0500

    pass

--- HackerRank/Algorithm/longly_integer.py
+++ HackerRank/Algorithm/longly_integer.py
@@ -1,9 +1,16 @@
 #!/bin/python3
 
 import sys
+import re
 
 def lonely_integer(a):
-    
+    s = set()
+    for i in a:
+    	if i in s:
+    		s.remove(i)
+    	else:
+    		s.add(i)
+    return(s.pop())
 
 n = int(input().strip())
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
