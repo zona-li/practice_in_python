9cd1f8084ce6c3c8bab7c517dcd5111a2e25b8f7
zona-li <haoyang.zona@gmail.com>
2022-07-01 11:33:23 -0500

    419

---
+++ LC/419.py
@@ -0,0 +1,10 @@
+def countBattleships(self, board):
+    total = 0
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 'X':
+                flag = 1
+                if j > 0 and board[i][j-1] == 'X': flag = 0
+                if i > 0 and board[i-1][j] == 'X': flag = 0
+                total += flag
+    return total
