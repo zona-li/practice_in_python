bda315a5f0471aa8e3e784ed897fcc47a6e55501
zona-li <haoyang.zona@gmail.com>
2022-07-08 20:46:38 -0500

    2115

--- LC/2115_find_recipes.py
+++ LC/2115_find_recipes.py
@@ -2,40 +2,35 @@
 # ingredients = [["sandwich","meat","bread"],["bread","meat"],["yeast","flour"]]
 # supplies = ["yeast","flour","meat"]
 
+from collections import Counter, defaultdict, deque
 from typing import List
+
+from traitlets import default
 
 
 class Solution:
   def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-    mem = {}
-    for supply in supplies:
-      mem[supply] = True
-    result = []
-    for idx, recipe in enumerate(recipes):
-      if mem.get(recipe):
-        result.append(recipe)
-        continue
-      q = ingredients[idx]
-      canMake = True
-      while len(q) and canMake:
-        next = q.pop()
-        available = mem.get(next)
-        if available:
-          continue
-        in_recipes = next in recipes
-        if not available and not in_recipes:
-          canMake = False
-          break
-        if in_recipes:
-          index = recipes.index(next)
-          q.extend(ingredients[index])
-      if canMake:
-        mem[recipe] = True
-        result.append(recipe)
-      else:
-        mem[recipe] = False
+    adj = defaultdict(set)
+    indegree = defaultdict(int)
 
-    return result
+    for rec, ings in zip(recipes, ingredients):
+      for ing in ings:
+        adj[ing].add(rec)
+        indegree[rec] += 1
+
+    res = []
+    q = deque(supplies)
+    while len(q):
+      next = q.popleft()
+      if next in recipes:
+        res.append(next)
+
+      for rec in adj[next]:
+        indegree[rec] -= 1
+        if indegree[rec] == 0:
+          q.append(rec)
+
+    return res
 
 
 
