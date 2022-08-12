0b8cea03097e2cb9b44b181d733f5b6369d53de1
zona-li <haoyang.zona@gmail.com>
2022-08-12 15:08:29 -0500

    139

---
+++ LC/139_word_break.py
@@ -0,0 +1,22 @@
+import enum
+from typing import List
+
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
+
+s = Solution()
+print(s.wordBreak('leetcode', ["leet","code"]))
+
+# "abcd"
+# ["a","abc","b","cd"]
