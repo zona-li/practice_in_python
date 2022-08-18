c9104ea1f370ed3a0d721e55eb169c941ee3c068
Zona <zona@zonas-air.lan>
2022-08-18 16:25:25 -0500

    1597

---
+++ LC/1597_binary_expression_tree.py
@@ -0,0 +1,10 @@
+# Definition for a binary tree node.
+class Node(object):
+    def __init__(self, val=" ", left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def expTree(self, s: str) -> 'Node':
+        pass
